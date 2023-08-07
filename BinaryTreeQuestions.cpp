cd95bd7f1dd687d556ec5d25f7f4caa86389567a
Lakshya Batra <lakshyabatra04@gmail.com>
2023-08-07 18:44:33 +0530

    ..
    .

--- BinaryTreeQuestions.cpp
+++ BinaryTreeQuestions.cpp
@@ -30,3 +30,34 @@ int height(node*root){
     int ans=max(left,right)+1;
     return ans;
 }
+//to obtain diameter of A GIVEN binary tree
+//approach 1
+//BAD solution due to TC of N*N 
+int diameter(node*root){
+    //base case
+    if(root==NULL){
+        return 0;
+    }
+    int opt1=diameter(root->left);
+    int opt2=diameter(root->right);
+    int opt3=height(root->left)+height(root->right)+1;
+    return max(max(opt1,opt2),opt3);
+}
+pair<int,int> diameterFast(node*root){
+    //base call
+    if(root==NULL){
+        pair<int,int> p=make_pair(0,0);
+        return p;
+    }
+    pair<int,int> left=diameterFast(root->left);
+    pair<int,int> right=diameterFast(root->right);
+    
+    int op1=left.first;
+    int op2=right.first;
+    int op3=left.second+right.second+1;
+
+    pair<int,int>ans;
+    ans.first=max(max(op1,op2),op3);
+    ans.second=max(left.second,right.second)+1;
+    return ans;
+}
